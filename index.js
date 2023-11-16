79bcba957ced72893879b76e68a1b89fe4ba56cc
Weverton Tenório Da Silva <36006938+WevertonTDS@users.noreply.github.com>
2023-11-15 21:22:22 -0300

    Projeto Concluído

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-let nome = prompt ("Digite o seu nome: ");
-console.log (nome);
+let nome = "Weverton";
+let nivel = 6002;
+
+if (nivel <= 1000) {
+    console.log("O Herói de nome: " + nome + ", está no nível Ferro!");
+    }
+
+    else if (nivel >= 1001 && nivel < 2000){
+    console.log("O Herói de nome: " + nome + ", está no nível Bronze!");
+    }
+
+    else if (nivel >= 2001 && nivel < 6000){
+    console.log("O Herói de nome: " + nome + ", está no nível Prata!");
+    }
+
+    else if (nivel >= 6001 && nivel < 7000){
+    console.log("O Herói de nome: " + nome + ", está no nível Ouro!");
+    }
+
+    else if (nivel >= 7001 && nivel < 8000){
+    console.log("O Herói de nome: " + nome + ", está no nível Platina!");
+    }
+
+    else if (nivel >= 8001 && nivel < 9000){
+    console.log("O Herói de nome: " + nome + ", está no nível Ascendente!");
+    }
+
+    else if (nivel >= 9001 && nivel < 10000){
+    console.log("O Herói de nome: " + nome + ", está no nível Imortal!");
+    }
+
+    else {
+    console.log("O Herói de nome: " + nome + ", está no nível Radiante");
+    }
